be7463485e6955e7cbd047c4ee792687a4ad3e1e
Ayush kumar <ayushmaurya496@gmail.com>
2026-08-19 13:59:18 +0530

    added mongodb and connected successfully

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const dns=require('dns')
+dns.setServers(['8.8.8.8','8.8.4.4'])
+const dotenv = require("dotenv")
+dotenv.config()
+const cors = require("cors")
+const connectDB = require("./src/config/db")
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, (req, res) => {
     console.log(`server is running on ${PORT}`)
