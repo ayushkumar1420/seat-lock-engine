76a2893f30c5d3cafbada60b2f993e9e2594cf3c
Ayush kumar <ayushmaurya496@gmail.com>
2026-08-26 15:07:03 +0530

    created a seat model to prevent from double-booking with seat inventory

---
+++ server/src/modules/seat/seat.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const seatSchema = new mongoose.Schema(
+    {
+        showtimeId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "Showtime"
+        },
+
+        seatNumber: {
+            type: String,
+            required: true,
+        },
+
+        status: {
+            type: String,
+            required: ["AVAILABLE", "BOOKED"],
+            default: "AVAILABLE",
+        },
+
+        bookingId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Booking",
+            default: null,
+        },
+    },
+
+    {
+        timestamps: true,
+    },
+);
+
+module.exports = mongoose.model("Seat", seatSchema)
