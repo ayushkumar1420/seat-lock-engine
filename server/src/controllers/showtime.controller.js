0c3611d5874b2b8df9a66bf0cd2aa1b2903f1072
Ayush kumar <ayushmaurya496@gmail.com>
2026-08-27 23:09:38 +0530

    added showtime details to make a session start

--- server/src/controllers/showtime.controller.js
+++ server/src/controllers/showtime.controller.js
@@ -3,5 +3,42 @@ const mongoose = require("mongoose");
 const Showtime = require("../modules/catalog/showtime.model");
 const createSeatInventory = require("../utils/createSeatInventory");
 
+const createShowtime = async (req, res) => {
+    const session = await mongoose.startSession();
+
+    try {
+        const {
+            movieId,
+            screenId,
+            startTime,
+            ticketPrice,
+        } = req.body
+
+        if ( !movieId || !screenId || !startTime || ticketPrice === undefined )
+        {
+            return res.status(400).json({
+                message: "all showtime fields are required",
+            });
+        }
+
+        session.startTransaction();
+
+        //create showtime
+        const [showtime] = await Showtime.create(
+            [
+                {
+                    movieId, screenId, startTime, ticketPrice,
+                },
+            ],
+            {
+                session,
+            }
+        );
+
+        
+    } catch (error) {
+        
+    }
+}
 
 module.exports = { createShowtime, }
