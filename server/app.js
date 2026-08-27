6e728b5a7eeb9bcdf7bb4ce59214d4c3745d02f1
Ayush kumar <ayushmaurya496@gmail.com>
2026-08-28 00:20:19 +0530

    connected the routes to the api.js to build the api connection between them

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
 const bookingRoutes = require('./src/modules/booking/booking.routes.js');
+const showtimeRoutes = require(".src/modules/catalog/showtime.routes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/bookings", bookingRoutes);
+app.use("/api/showtimes", showtimeRoutes);
 
 module.exports = app;
