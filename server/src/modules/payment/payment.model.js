8d778212eeb29f9309157c3bf61349f25aa59aa1
Ayush kumar <ayushmaurya496@gmail.com>
2026-08-28 17:08:53 +0530

    created a payment model that becomes the permanent record of the financial side of a booking

---
+++ server/src/modules/payment/payment.model.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema(
+    {
+        bookingId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Booking",
+            required: true,
+            index: true,
+        },
+
+        userId: {
+            type: String,
+            required: true,
+        },
+
+        amount: {
+            type: Number,
+            required: true,
+        },
+
+        currency: {
+            type: String,
+            default: "INR",
+        },
+
+        status: {
+            type: String,
+            enum: [ "CREATED", "SUCCESS", "FAILED", "REFUNDED"],
+            default: "CREATED",
+        },
+
+        razorpayOrderId: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+
+        razorpayPaymentId: {
+            type: String,
+            default: null,
+        },
+
+        razorpaySignature: {
+            type: String,
+            default: null,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("Payment", paymentSchema);
