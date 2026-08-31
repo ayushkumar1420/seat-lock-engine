6089595fed67ffd6253e7eab52de21bedb9cc6a6
Ayush kumar <ayushmaurya496@gmail.com>
2026-08-31 17:07:02 +0530

    connected it in the app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const bookingRoutes = require('./src/modules/booking/booking.routes.js');
 const showtimeRoutes = require("./src/modules/catalog/showtime.routes.js");
+const paymentRoutes = require("./src/modules/payment/payment.routes.js");
+
 
 const app = express();
 
@@ -9,5 +11,6 @@ app.use(express.json());
 
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/showtimes", showtimeRoutes);
+app.use("/api/payments", paymentRoutes);
 
 module.exports = app;
