2c94801a9cdde92e844ce058d78fde0e85dffdf5
Ayush kumar <ayushmaurya496@gmail.com>
2026-09-01 20:12:28 +0530

    interegated into the app.js and api called through app.js

--- server/app.js
+++ server/app.js
@@ -3,9 +3,13 @@ const express = require("express");
 const bookingRoutes = require('./src/modules/booking/booking.routes.js');
 const showtimeRoutes = require("./src/modules/catalog/showtime.routes.js");
 const paymentRoutes = require("./src/modules/payment/payment.routes.js");
+const paymentWebhookRoutes = require("./src/modules/payment/payment.webhook.routes.js");
 
 
 const app = express();
+
+//webhook pehle call hoga qki it needs raw body
+app.use("/api/webhooks", paymentWebhookRoutes);
 
 app.use(express.json());
 
