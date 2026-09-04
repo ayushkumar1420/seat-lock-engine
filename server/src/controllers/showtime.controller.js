f0312b2b1a33209a51977e7224caecee19c96a41
Ayush kumar <ayushmaurya496@gmail.com>
2026-09-04 23:19:49 +0530

    added a seat inventory endpoint to get showtime seats

--- server/src/controllers/showtime.controller.js
+++ server/src/controllers/showtime.controller.js
@@ -64,4 +64,37 @@ const createShowtime = async (req, res) => {
     }
 }
 
-module.exports = { createShowtime, }
+const getShowtimeSeats = async (req, res) => {
+    try {
+        const { showtimeId } = req.params;
+        const showtime = await Showtime.findById(showtimeId);
+
+        if(!showtime) {
+            return res.status(404).json({
+                message: "showtime not found",
+            });
+        }
+
+        const seats = await seatModel.find({showtimeId})
+        .select("seatnumber status")
+        .sort({seatNumber: 1});
+
+        return res.status(200).json({
+            showtimeId: showtime._id,
+            startTime: showtime.startTime,
+            ticketPrice: showtime.ticketPrice,
+            seats,
+        });
+
+    } catch (error) {
+         console.error(
+            "Get showtime seats error:",
+            error
+        );
+
+        return res.status(500).json({
+            message: "Failed to fetch seats",
+        });
+    }
+}
+module.exports = { createShowtime, getShowtimeSeats }
