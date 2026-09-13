349f8e5c6a259a9524a9834a3625a858bb09f292
Ayush kumar <ayushmaurya496@gmail.com>
2026-09-13 12:37:21 +0530

    connect routes in the booking routes.js

--- server/src/modules/booking/booking.routes.js
+++ server/src/modules/booking/booking.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { lockSeats, confirmBooking } = require("../../controllers/booking.controller");
+const { lockSeats, confirmBooking, getBookingStatus } = require("../../controllers/booking.controller");
 
 const router = express.Router();
 
 router.post("/lock", lockSeats);
 router.post("/confirm", confirmBooking);
+router.get("/:bookingId/status", getBookingStatus);
 
 module.exports = router;
