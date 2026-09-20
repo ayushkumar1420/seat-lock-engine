989330c086d1807a4460ee930d21e4326e8fcc8d
Ayush kumar <ayushmaurya496@gmail.com>
2026-09-20 21:30:17 +0530

    connected in app.js of server authRoutes

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const bookingRoutes = require('./src/modules/booking/booking.routes.js');
 const showtimeRoutes = require("./src/modules/catalog/showtime.routes.js");
 const paymentRoutes = require("./src/modules/payment/payment.routes.js");
 const paymentWebhookRoutes = require("./src/modules/payment/payment.webhook.routes.js");
+const authRoutes = require("./src/modules/auth/auth.routes.js");
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use("/api/webhooks", paymentWebhookRoutes);
 
 app.use(express.json());
 
+app.use("/api/auth", authRoutes)
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/showtimes", showtimeRoutes);
 app.use("/api/payments", paymentRoutes);
